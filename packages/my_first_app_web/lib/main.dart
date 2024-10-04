bb9097d216b641aa0fb29bbea335b33ba5fca21b
“FelipeQueFez“ <felipequefez@gmail.com>
2024-10-04 20:45:26 +0100

    chore:widgets

--- packages/my_first_app_web/lib/main.dart
+++ packages/my_first_app_web/lib/main.dart
@@ -13,11 +13,65 @@ class MyApp extends StatelessWidget {
           title: Text('Meu primeiro app flutter'),
         ),
         body: Center(
-          child: Text(
-            "Ola, Flutter 2",
-            style: TextStyle(
-              fontSize: 26,
-            ),
+          child: Column(
+            children: [
+              Text(
+                'Ola, Flutter',
+                style: TextStyle(
+                  fontSize: 26,
+                ),
+              ),
+              Row(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    child: Text(
+                      "container 1",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.red,
+                    child: Text(
+                      "container 2",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.purple,
+                child: Text('Container 3'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('botao pressionado');
+                },
+                child: Text('Clique aqui'),
+              ),
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Mapa'),
+              ),
+              ListTile(
+                leading: Icon(Icons.phone),
+                title: Text('Phone'),
+              ),
+              ListTile(
+                leading: Icon(Icons.pages),
+                title: Text('Pages'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your username',
+                ),
+              ),
+            ],
           ),
         ),
       ),
